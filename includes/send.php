1c37dd12ff09c0c69289ae222cdc999081f0f4d1
strykes <renebvip@hotmail.fr>
2015-09-12 11:27:05 +0200

    Update and rename send.php to includes/send.php

--- includes/send.php
+++ includes/send.php
@@ -1,9 +1,9 @@
 <?php
-include("functions.php");
-$started = false;
+include( "functions.php" );
 $data = pack("VV",1,03).$config["server_rcon"].chr(0).''.chr(0);
 $data = pack("V",strlen($data)).$data;
 fwrite($conn, $data, strlen($data));
 $size = @fread($conn, 4);
 sendcmd($conn, $_POST["cmd"]);
+echo $_POST["cmd"];
 ?>
